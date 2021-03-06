63a46854a62d043365c875d7bf8d47678f2ce493
dominikfreund <dominik.freund99@gmail.com>
2021-03-06 13:21:26 +0100

    Änderung 102.4 Entfernen der Hilfsvariablen

--- src/main/java/dhbw/fowler1/videostore/Customer.java
+++ src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -21,8 +21,6 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
 
-            thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -31,8 +29,8 @@ public class Customer {
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
